9a742c188bf99cca1c6043f8c7fd92e01cea2dfa
OforiMcke <oforimckeownjulius@gmail.com>
2026-08-14 11:18:43 +0000

    changes in axiosInstance

--- frontend/application-portal/src/api/axiosInstance.js
+++ frontend/application-portal/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create();
+export const api = axios.create({ baseURL: "/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("authToken");
