cc1163eaeb6c19fd65732ecf2499ac305b6089d2
OforiMcke <oforimckeownjulius@gmail.com>
2026-08-14 16:51:13 +0000

    another origin fixx

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL ? process.env.CLIENT_URL.split(",") : "*",
   }),
 );
 app.use(express.json());
